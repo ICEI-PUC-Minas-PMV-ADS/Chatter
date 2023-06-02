6770efafd121255d49ab5581865cdb762465bac0
Allana <allanatavaresb@gmail.com>
2023-06-02 19:11:01 -0300

    RegisterPage

--- src/AppMobile/src/Pages/RegisterPage.js
+++ src/AppMobile/src/Pages/RegisterPage.js
@@ -9,39 +9,75 @@ import {
   Image,
   Dimensions,
   KeyboardAvoidingView,
+  AsyncStorage,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import axios from "axios";
 
 const { width, height } = Dimensions.get("screen");
 
 const RegisterPage = ({ navigation }) => {
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [buttonColor, setButtonColor] = useState("#f7b731");
-  const [isButtonPressed, setIsButtonPressed] = useState(false);
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [validationErrors, setValidationErrors] = useState([]);
 
+  const handleValidation = () => {
+    const errorMessages = [];
 
-  const handleRegister = () => {
-    // callApi(email, password)
+    if (password !== confirmPassword) {
+      errorMessages.push("A senha e a senha de confirmação devem ser iguais.");
+    }
+    if (username.length < 3) {
+      errorMessages.push("O nome de usuário deve ter mais de 3 caracteres.");
+    }
+    if (password.length < 8) {
+      errorMessages.push("A senha deve ter pelo menos 8 caracteres.");
+    }
+    if (email === "") {
+      errorMessages.push("Email é obrigatório.");
+    }
+
+    setValidationErrors(errorMessages);
+    return errorMessages.length === 0;
   };
+
+  const handleRegister = async () => {
+    if (handleValidation()) {
+      try {
+        const response = await axios.post(registerRoute, {
+          username,
+          email,
+          password,
+        });
+
+        const data = response.data;
+
+        if (data.status === false) {
+          // Display error message
+          console.error(data.msg);
+        }
+        if (data.status === true) {
+          // Store data locally using AsyncStorage or another storage solution
+          await AsyncStorage.setItem("@MyApp:user", JSON.stringify(data.user));
+
+          // Navigate to the desired screen or perform any other action
+          navigation.navigate("SetAvatar");
+        }
+      } catch (error) {
+        // Handle error
+        console.error(error);
+      }
+    }
+  };
+
 
   const handleSignIn = () => {
     navigation.navigate('LoginPage')
   };
 
   const colors = ["#00BCD4", "#7870D0", "#8468D0", "#985BCF"];
-
-  const handleButtonPress = () => {
-    setIsButtonPressed(true);
-    setButtonColor("purple");
-
-    setTimeout(() => {
-      setIsButtonPressed(false);
-      setButtonColor("colors");
-      handleRegister();
-      navigation.navigate("SetAvatar");
-    }, 2000);
-  };
 
   return (
     <View
@@ -88,14 +124,43 @@ const RegisterPage = ({ navigation }) => {
               >
                 <TextInput
                   style={{
+                    padding: 10,
+                    borderRadius: 4,
+                    borderWidth: 1,
                     backgroundColor: "#FFF",
                     width: "100%",
-                    padding: 10,
                   }}
                   placeholder="Usuário"
+                  value={username}
+                  onChangeText={setUsername}
+                />
+                {validationErrors.includes("O nome de usuário deve ter mais de 3 caracteres.") && (
+        <Text style={{ color: "red" }}>O nome de usuário deve ter mais de 3 caracteres.</Text>
+      )}
+              </LinearGradient>
+            </View>
+            <View style={{ height: 30 }} />
+            <View>
+              <LinearGradient
+                style={{
+                  padding: 1,
+                }}
+                colors={colors}
+              >
+                <TextInput
+                  style={{
+                    padding: 10,
+                    borderRadius: 4,
+                    borderWidth: 1,
+                    backgroundColor: "#FFF",
+                    width: "100%",
+                  }}
+                  placeholder="seu@email.com"
                   value={email}
                   onChangeText={setEmail}
-                />
+                />{validationErrors.includes("Email é obrigatório.") && (
+                  <Text style={{ color: "red" }}>Email é obrigatório.</Text>
+                )}
               </LinearGradient>
             </View>
             <View style={{ height: 30 }} />
@@ -122,6 +187,35 @@ const RegisterPage = ({ navigation }) => {
               </LinearGradient>
             </View>
             <View style={{ height: 30 }} />
+            <View>
+              <LinearGradient
+                style={{
+                  padding: 1,
+                }}
+                colors={colors}
+              >
+                <TextInput
+                  style={{
+                    padding: 10,
+                    borderRadius: 4,
+                    borderWidth: 1,
+                    backgroundColor: "#FFF",
+                    width: "100%",
+                  }}
+                  placeholder="Confirme sua Senha"
+                  secureTextEntry
+                  value={confirmPassword}
+                  onChangeText={setConfirmPassword}
+                />{validationErrors.includes("A senha deve ter pelo menos 8 caracteres.") && (
+                  <Text style={{ color: "red" }}>A senha deve ter pelo menos 8 caracteres.</Text>
+                )}
+          
+                {validationErrors.includes("A senha e a senha de confirmação devem ser iguais.") && (
+                  <Text style={{ color: "red" }}>A senha e a senha de confirmação devem ser iguais.</Text>
+                )}
+              </LinearGradient>
+            </View>
+            <View style={{ height: 30 }} />
             <TouchableOpacity
               style={{
                 paddingVertical: 10,
@@ -129,7 +223,7 @@ const RegisterPage = ({ navigation }) => {
                 alignItems: "center",
                 justifyContent: "center",
                 }}
-                onPress={handleButtonPress}
+                onPress={handleRegister}
             >
               <LinearGradient
                 style={{
